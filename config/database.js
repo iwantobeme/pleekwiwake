d25e7c1998b5f941ed73a674e092739c765a2216
Panuwat Yuenyong <59143204@g.cmru.ac.th>
2019-07-21 15:33:51 +0700

    addpassphp

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   multipleStatements: true,
   host: "localhost",
   user: "root",
-  password: "",
+  password: "5Em9b6h0NzWdJjq5git",
   database: "webwat"
 });
 export default connection;
